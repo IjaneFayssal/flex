7cca007f69c9e1b445548cedc7f33351cf03b14e
IjaneFayssal <76232773+IjaneFayssal@users.noreply.github.com>
2021-02-12 15:55:32 +0100

    Update flex.js

--- flex.js
+++ flex.js
@@ -1,8 +1,9 @@
 function stampa() {
+        document.getElementById("id01").style.display="block";
         var x = document.getElementById("skusku");
         var flex="";
         for (var i = 0; i < 10; i++) {
             flex = flex+("blablabla: " + i + "<br>");
         }
         x.innerHTML=flex;
-    }
+        }
